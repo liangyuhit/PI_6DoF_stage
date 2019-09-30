81607ab30856818c20d4cac58ca774f50ac1b374
liangyuhit <312592076@qq.com>
2019-09-30 09:45:44 +0200

    2019-9-30 09:45:42

--- Measure_scanline.py
+++ Measure_scanline.py
@@ -37,12 +37,12 @@ RECRATE = 47.6  # number of recordings per second, i.e. in Hz
 '''
 NUMPOINTS = 30000  # number of points for one sine period as integer
 STARTPOS = 0.0  # start position of the circular motion as float for both axes
-# AMPLITUDE = 45   # amplitude of the circular motion as float for both axes
-AMPLITUDE = 200
+AMPLITUDE = 45   # amplitude of the circular motion as float for both axes
+# AMPLITUDE = 200
 # if AMPLITUDE >= 50:
 #     print('AMPLITUDE TOO LARGE')
 #     exit()
-OFFSET = -100
+OFFSET = 0
 NUMCYLES = 1  # number of cycles for wave generator output
 # TABLERATE = 1  # duration of a wave table point in multiples of servo cycle times as integer
 TABLERATE = 25
@@ -53,7 +53,7 @@ Data_record_Table_rate = 12 ### 47.6Hz; ~12 for TABLERATE 25; ~60 for TABLERATE 
 '''
 ### 1=x,2=y,3=z_rot,4=z,5=x_rot,6=y_rot
 
-moving_axis = 5
+moving_axis = 2
 
 wavegens = (1, 2, 3, 4, 5, 6)
 wavetables = (1, 2, 3, 4, 5, 6)
@@ -69,7 +69,14 @@ with GCSDevice(CONTROLLERNAME) as pidevice:
     pitools.startup(pidevice, STAGES, REFMODE)
     IDN = pidevice.qIDN()
     print('IDN: ', IDN)
+    
+#     '''
+#         Turn off controller
+#     '''
+#     for axes in range(6):
+#         pidevice.SVO(axes=axes+1, values=False)
     print('Servo Status: ', pidevice.qSVO())
+    
     pidevice.WGO(wavegens, mode=[0]*len(wavegens))
     '''
         Auto-Zero
@@ -95,14 +102,14 @@ with GCSDevice(CONTROLLERNAME) as pidevice:
     drec.arm()
     print(pidevice.qDRT())
     print('Sampling Freq. = ', drec.samplefreq)
-
+ 
     '''
         Wave Generator Configuration
     '''
 #     print('Wave Generator Num. ', pidevice.qTWG())
 #     Servo_update_time = pidevice.qSPA(items=1, params=0x0E000200)[1][234881536]### 0x0E000200
 #     print('Servo update time: /s', Servo_update_time)
-      
+       
     pidevice.WAV_LIN(table=wavetables[moving_axis-1], firstpoint=1, numpoints=NUMPOINTS, append='X',
                     speedupdown=NUMPOINTS//10, amplitude=AMPLITUDE, offset=OFFSET, seglength=NUMPOINTS)
 #     pidevice.WAV_SIN_P(table=wavetables[1], firstpoint=1, numpoints=NUMPOINTS, append='X',
@@ -112,7 +119,7 @@ with GCSDevice(CONTROLLERNAME) as pidevice:
     pidevice.WSL(wavegens, wavetables)
     pidevice.WGC(wavegens, [NUMCYLES]*len(wavegens))
     pidevice.WTR(0, tablerates=TABLERATE, interpol=1)
-    
+     
     '''
         Trigger Configuration
     '''
@@ -121,7 +128,7 @@ with GCSDevice(CONTROLLERNAME) as pidevice:
 #     for i in range(NUMPOINTS//60+1): ### 50Hz~60 for TABLERATE 5
         pidevice.TWS(lines=2, points=1+Data_record_Table_rate*i, switches=1) ### 50Hz~12 for TABLERATE 25
 #         pidevice.TWS(lines=2, points=1+60*i, switches=1) ### 50Hz~60 for TABLERATE 5
-    
+     
     pidevice.CTO(lines=2, params=1, values=0.1)
 #     pidevice.CTO(lines=2, params=2, values=2)
     pidevice.CTO(lines=2, params=3, values=9)
@@ -133,25 +140,25 @@ with GCSDevice(CONTROLLERNAME) as pidevice:
     print('Trigger step = ', float(Trig_step))
     print('Trigger line = ', Trig_line)
     print('Trigger mode = ', Trig_mode)
-     
+      
 #     print('Data recorder options: ', pidevice.qHDR())
-     
+      
     pidevice.DRC(tables=1, sources='2', options=2)
     pidevice.DRC(tables=2, sources='3', options=2)
     pidevice.DRC(tables=3, sources='5', options=2)
     print('Data recorder configuration: ', pidevice.qDRC())
-    
+     
     print('Record Table Rate: ', pidevice.qRTR())
 #     pidevice.DRT(tables=1, sources='1', values=1)
 #     print('Data recorder TriggerSource: ', pidevice.qDRT())
 #     pitools.waitonready(pidevice)
-    
+     
 #     Table_rate = pidevice.qSPA(items=1, params=0x13000109)[1][318767369] ###0x13000109
 #     print(Table_rate)
 #     print(pidevice.qWTR(wavegens=1))
     pitools.waitontarget(pidevice, '%s'%moving_axis)
     print('Servo Status: ', pidevice.qSVO())
-    
+     
     '''
         Notice the Axis No.2!!!!
     '''
@@ -161,66 +168,66 @@ with GCSDevice(CONTROLLERNAME) as pidevice:
         time.sleep(1.0)
     print('done')
     pidevice.WGO(wavegens[moving_axis-1], mode=[0])
-
+ 
 #     time.sleep(2.0)
 #     pidevice.WGO(wavegens=1, mode=0)
-    
+     
     '''
         Set Target Relative To Current Position
     '''
 #     pidevice.MVR('2', 0.1) ### y = 2
-    
+     
     '''
         Get Target Position
     '''
 #     target_position = pidevice.qMOV() 
 #     target_x, target_y, target_z_r, target_z, target_x_r, target_y_r = pidevice.qMOV()['1'],pidevice.qMOV()['2'],pidevice.qMOV()['3'],pidevice.qMOV()['4'],pidevice.qMOV()['5'],pidevice.qMOV()['6']
 #     print(target_x, target_y, target_z_r, target_z, target_x_r, target_y_r)
-    
+     
     '''
         Get Real Position
     '''
 #     pos = pidevice.qPOS()
 #     pos_x, pos_y, pos_z_r, pos_z, pos_x_r, pos_y_r = pidevice.qMOV()['1'],pidevice.qMOV()['2'],pidevice.qMOV()['3'],pidevice.qMOV()['4'],pidevice.qMOV()['5'],pidevice.qMOV()['6']
 #     print(pos_x, pos_y, pos_z_r, pos_z, pos_x_r, pos_y_r)
-    
-    
-    
-
+     
+     
+     
+ 
     '''
         Data Recording
     '''
-    
+     
     header, data = drec.getdata()
-   
+    
     y_pos, z_rot, x_rot = data[0], data[1], data[2]
-       
+        
     samp_time = NUMVALUES/RECRATE
     n_data = NUMVALUES
     print('Sampling Rate = ', RECRATE)
     print('Data length = ', n_data)
     print('Time = ', samp_time)
-      
-
+       
+ 
 #     header, data = pidevice.qDRR(tables=[1,2,3], offset=1, numvalues=NUMVALUES)
-    
+     
 #     header, data = datarectools.Datarecorder(pidevice).read(offset=1, numvalues=NUMVALUES)
 #     print('Num. of recorded points: ', pidevice.qDRL())
 #     samp_time = NUMVALUES/RECRATE
 #     n_data = NUMVALUES
-    
+     
 #     print(header)
 #     print((data))
 # #     y_pos, z_rot, x_rot = data[0], data[1], data[2]
 #     y_pos = data[0]
-    
+     
 #     print('Sampling Rate = ', RECRATE)
 #     print('Data length = ', n_data)
 #     print('Time = ', samp_time)
 #     print(len(y_pos))
-
-
-
+ 
+ 
+ 
 ''' 
     保存文件 Exporting TXT
 '''
@@ -236,41 +243,41 @@ header = ['%s\n' %(name+'_PI'),
       '-------------------------------------------------\n',
       ]
 out_str = ['%f, %f, %f\n' %(y_pos[i], z_rot[i], x_rot[i]) for i in range(NUMVALUES)]
-
+ 
 '''
     Output
 '''
 Export_Data(PI_name, header, out_str)
 print('TXT file saved')
-
+ 
 t = np.linspace(0, samp_time, num=n_data)
-
+ 
 plt.figure(1)
 plt.gcf().set_size_inches(18,9)
-
+ 
 plt.subplot(3,1,1)
 plt.plot(t, y_pos, color='blue', label='length')
 plt.title('Length')
 plt.xlabel('Time /s')
 plt.ylabel('Position /nm')
 plt.grid(which='both', axis='both')
-
+ 
 plt.subplot(3,1,2)
 plt.plot(t, z_rot, color='red', label='Hor_Angle')
 plt.grid(which='both', axis='both')
 plt.xlabel('Time /s')
 plt.ylabel('Angle /urad')
 plt.title('Hor_Angle')
-
+ 
 plt.subplot(3,1,3)
 plt.plot(t, x_rot, color='black', label='Ver_Angle')
 plt.grid(which='both', axis='both')
 plt.title('Ver_Angle')
 plt.xlabel('Time /s')
 plt.ylabel('Angle /urad')
-
+ 
 figManager = plt.get_current_fig_manager()
 figManager.window.showMaximized()
 plt.tight_layout()
-
+ 
 plt.show()
